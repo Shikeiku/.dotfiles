1e4082f1a15b1ce2f5dc5497665a23c5fc7102a8
Mike Vink <mikevink@MacBook-Air.domain_not_set.invalid>
2020-08-26 00:50:31 +0200

    Just added the env file

---
+++ env/env.sh
@@ -0,0 +1,7 @@
+# Give the XDG dirs
+export XDG_CONFIG_HOME=~/.dotfiles
+export XDG_DATA_HOME=~/.data
+export XDG_CACHE_HOME=~/.cache
+
+# Update the PATH for user bin
+export PATH=$PATH:/Users/mike/bin
